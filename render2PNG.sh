96a92b94b7270595c4e2e0ca5a2805f37abe321d
b3479761 <73123086+b3479761@users.noreply.github.com>
2020-10-30 08:30:18 +0000

    Add files via upload
    For raspbian git clone https://github.com/b3479761/downloadwaifu.git

---
+++ render2PNG.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+blender -b /home/pi/downloadwaifu/mikuN2lowPolyGreeting.blend --python /home/pi/downloadwaifu/bpy2frames.py
---
+++ startwaifu.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr -s 640x480
+wait 30
+blender /home/pi/downloadwaifu/mikuN2lowPolyGreeting.blend
